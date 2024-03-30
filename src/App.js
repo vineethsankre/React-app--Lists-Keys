f82e34dd42775e97501f70b7dbde5c65c03aa8fb
vineethsankre <lsvineethsankre@gmail.com>
2024-03-30 15:58:15 +0000

    modified role

--- src/App.js
+++ src/App.js
@@ -3,25 +3,25 @@ import './App.css'
 
 const userDetailsList = [
   {
-    uniqueNo:1,
+    uniqueNo: 1,
     imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
     name: 'Ester Howard',
     role: 'Software Engineer',
   },
   {
-    uniqueNo:2,
+    uniqueNo: 2,
     imageUrl: 'https://assets.ccbp.in/frontend/react-js/floyd-miles-img.png',
     name: 'Floyd miles',
     role: 'Business Analyst',
   },
   {
-    uniqueNo:3,
+    uniqueNo: 3,
     imageUrl: 'https://assets.ccbp.in/frontend/react-js/jacob-jones-img.png',
     name: 'Jacob Jones',
-    role: 'DevOps Engineer',
+    role: 'Java Developer',
   },
   {
-    uniqueNo:4,
+    uniqueNo: 4,
     imageUrl: 'https://assets.ccbp.in/frontend/react-js/devon-lane-img.png',
     name: 'Devon lane',
     role: 'Jr. Software Engineer',
@@ -29,11 +29,11 @@ const userDetailsList = [
 ]
 
 const App = () => (
-  <div className = "list-container">
-    <h1 className = "title">Users List</h1>
+  <div className="list-container">
+    <h1 className="title">Users List</h1>
     <ul>
       {userDetailsList.map(eachItem => (
-        <UserProfile userDetails = {eachItem} key = {eachItem.uniqueNo}/>
+        <UserProfile userDetails={eachItem} key={eachItem.uniqueNo} />
       ))}
     </ul>
   </div>

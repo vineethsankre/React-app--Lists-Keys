e098edac2a191b4966bc75841ffdc59f77df1c7c
vineethsankre <lsvineethsankre@gmail.com>
2024-03-30 15:56:00 +0000

    List of profiles added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import UserProfile from '.components/UserProfile/index'
+import './App.css'
 
-const userDetails = {
-  imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
-  name: 'Ester Howard',
-  role: 'Software Engineer',
-}
+const userDetailsList = [
+  {
+    uniqueNo:1,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'Ester Howard',
+    role: 'Software Engineer',
+  },
+  {
+    uniqueNo:2,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/floyd-miles-img.png',
+    name: 'Floyd miles',
+    role: 'Business Analyst',
+  },
+  {
+    uniqueNo:3,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/jacob-jones-img.png',
+    name: 'Jacob Jones',
+    role: 'DevOps Engineer',
+  },
+  {
+    uniqueNo:4,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/devon-lane-img.png',
+    name: 'Devon lane',
+    role: 'Jr. Software Engineer',
+  },
+]
 
-const App = () => <UserProfile userDetails={userDetails} />
+const App = () => (
+  <div className = "list-container">
+    <h1 className = "title">Users List</h1>
+    <ul>
+      {userDetailsList.map(eachItem => (
+        <UserProfile userDetails = {eachItem} key = {eachItem.uniqueNo}/>
+      ))}
+    </ul>
+  </div>
+)
 
 export default App
--- src/Components/UserProfile/index.js
+++ src/Components/UserProfile/index.js
@@ -1,14 +1,14 @@
 import './index.css'
 
 const UserProfile = props => {
-  const {userDetails} = props
+  const {userDetails, key} = props
   const {imageUrl, name, role} = userDetails
   return (
     <div className="user-card-container">
       <img className="avatar" src={imageUrl} alt="avatar" />
       <div className="user-details-container">
         <h1 className="user-name">{name}</h1>
-        <p className= "user-designation">{role}</p>
+        <p className="user-designation">{role}</p>
       </div>
     </div>
   )

5262ee5029330fc92c64bc6f62487c27a1c9f6cd
vineethsankre <lsvineethsankre@gmail.com>
2024-03-30 15:44:34 +0000

    Single profile created

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
-const App = () => <h1>Users List App</h1>
+import UserProfile from '.components/UserProfile/index'
+
+const userDetails = {
+  imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+  name: 'Ester Howard',
+  role: 'Software Engineer',
+}
+
+const App = () => <UserProfile userDetails={userDetails} />
 
 export default App
